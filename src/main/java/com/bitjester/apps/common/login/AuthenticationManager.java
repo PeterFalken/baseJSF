522dfd526ec6674db508d5777f08f4d338aad51d
PeterFalken <luis@bitjester.com>
2014-11-24 11:43:34 -0600

    Updated facelets.

--- src/main/java/com/bitjester/apps/common/login/AuthenticationManager.java
+++ src/main/java/com/bitjester/apps/common/login/AuthenticationManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
@@ -13,6 +14,7 @@ import com.bitjester.apps.common.entities.AppUser;
 import com.bitjester.apps.common.utils.BookKeeper;
 import com.bitjester.apps.common.utils.HashUtil;
 
+@Named
 @Stateless
 public class AuthenticationManager implements Serializable {
     private static final long serialVersionUID = 1L;
