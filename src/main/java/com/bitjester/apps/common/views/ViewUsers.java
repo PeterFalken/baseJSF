bebeb8c3126aab4018206bd7ab882e12b3efeba3
PeterFalken <luis@bitjester.com>
2014-11-25 16:26:10 -0600

    New ViewUsers - using the BaseView. User roles are now controlled from view.

--- src/main/java/com/bitjester/apps/common/views/ViewUsers.java
+++ src/main/java/com/bitjester/apps/common/views/ViewUsers.java
@@ -1,8 +1,13 @@
 package com.bitjester.apps.common.views;
 
-import java.io.Serializable;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.List;
+
+import com.bitjester.apps.common.BaseView;
+import com.bitjester.apps.common.entities.AppUser;
+import com.bitjester.apps.common.utils.FacesUtil;
+import com.bitjester.apps.common.utils.HashUtil;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -10,95 +15,95 @@ import javax.enterprise.inject.Produces;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-
-import com.bitjester.apps.common.entities.AppUser;
-import com.bitjester.apps.common.utils.BookKeeper;
-import com.bitjester.apps.common.utils.FacesUtil;
-import com.bitjester.apps.common.utils.HashUtil;
 
 @Named
 @ViewScoped
-public class ViewUsers implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class ViewUsers extends BaseView {
+    @Inject
+    private String appName;
+    private AppUser managedUser;
+    private Map<String, String> roles;
 
-	@Inject
-	private String appName;
+    // ======= Init Method ======================
 
-	@Inject
-	private BookKeeper bk;
+    @PostConstruct
+    public void init() {
+        roles = new LinkedHashMap<>();
+        roles.put("Admin", "admin");
+        roles.put("System Admin", "sadmin");
+        roles.put("User", "user");
+    }
 
-	@Inject
-	private EntityManager em;
+    // ======= User List Methods ================
+    @Named
+    @Produces
+    @RequestScoped
+    public List<String> getUserStartLetters() throws Exception {
+        String query = "SELECT DISTINCT SUBSTRING(username,1,1) AS letter FROM AppUser";
+        query += " ORDER BY letter";
+        return em.createQuery(query, String.class).getResultList();
+    }
 
-	private AppUser managedUser;
+    @RequestScoped
+    public List<AppUser> usersForLetter(String letter) throws Exception {
+        String query = "FROM AppUser WHERE username LIKE '" + letter + "%'";
+        query += " ORDER BY username";
+        return em.createQuery(query, AppUser.class).getResultList();
+    }
 
-	// ================================
-	// ======= Users Methods ==========
-	// ================================
+    // ======= Property methods =======
+    @Named
+    @Produces
+    public AppUser getManagedUser() {
+        return managedUser;
+    }
 
-	@Named
-	@Produces
-	@RequestScoped
-	public List<String> getUserStartLetters() throws Exception {
-		String query = "SELECT DISTINCT SUBSTRING(username,1,1) AS letter FROM AppUser";
-		query += " ORDER BY letter";
-		return em.createQuery(query, String.class).getResultList();
-	}
+    public void setManagedUser(AppUser managedUser) {
+        this.managedUser = managedUser;
+    }
 
-	@RequestScoped
-	public List<AppUser> usersForLetter(String letter) throws Exception {
-		String query = "FROM AppUser WHERE username LIKE '" + letter + "%'";
-		query += " ORDER BY username";
-		return em.createQuery(query, AppUser.class).getResultList();
-	}
+    public Map<String, String> getRoles() {
+        return roles;
+    }
 
-	// -- Persistence & form methods
-	@Named
-	@Produces
-	public AppUser getManagedUser() {
-		return managedUser;
-	}
+    // ======= Persistence methods ==============
+    public void load(Long id) throws Exception {
+        managedUser = em.find(AppUser.class, id);
+        managedUser.setActiveRole(managedUser.getAppRole(appName));
+    }
 
-	public void setManagedUser(AppUser managedUser) {
-		this.managedUser = managedUser;
-	}
+    public void newInstance() {
+        managedUser = new AppUser();
+    }
 
-	public void load(Long id) throws Exception {
-		managedUser = em.find(AppUser.class, id);
-		managedUser.setActiveRole(managedUser.getAppRole(appName));
-	}
+    public void refresh() throws Exception {
+        managedUser = null;
+    }
 
-	public void newInstance() {
-		managedUser = new AppUser();
-	}
+    public void remove(Long id) {
+        try {
+            bk.remove(em.find(AppUser.class, id));
+        } catch (Exception e) {
+            log.severe(e.getMessage());
+            FacesUtil.addMessage("An error occurred, please try again.");
+        }
+    }
 
-	public void refresh() throws Exception {
-		managedUser = null;
-	}
-
-	public void remove(Long id) {
-		try {
-			bk.remove(em.find(AppUser.class, id));
-		} catch (Exception e) {
-			FacesUtil.addMessage("Error ocurred, please reload page and try again.");
-		}
-	}
-
-	public void store() {
-		try {
-			if (null == managedUser.getId()) {
-				// New user will be assigned a 'user' role and default password.
-				managedUser.setAppRole(appName, "user");
-				managedUser.setPassword(HashUtil.calc_HashSHA("123456"));
-			} else {
-				// For existing users we update their role if necessary.
-				managedUser.setAppRole(appName, managedUser.getActiveRole());
-			}
-			bk.store(managedUser);
-			managedUser = null;
-		} catch (Exception e) {
-			FacesUtil.addMessage("Error ocurred, please reload page and try again.");
-		}
-	}
+    public void store() {
+        try {
+            if (null == managedUser.getId()) {
+                // New user will be assigned a 'user' role and default password.
+                managedUser.setAppRole(appName, "user");
+                managedUser.setPassword(HashUtil.calc_HashSHA("123456"));
+            } else {
+                // For existing users we update their role if necessary.
+                managedUser.setAppRole(appName, managedUser.getActiveRole());
+            }
+            bk.store(managedUser);
+            managedUser = null;
+        } catch (Exception e) {
+            log.severe(e.getMessage());
+            FacesUtil.addMessage("An error occurred, please try again.");
+        }
+    }
 }
